796483aeaa8ee50887aba5acf7fde61e2f76fbe0
riyazriya010 <riyur017@gmail.com>
2026-09-08 01:25:44 +0530

    app logo updated

--- client/src/app/manifest.ts
+++ client/src/app/manifest.ts
@@ -6,27 +6,27 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "Butterfly Bakes",
     description:
       "Delicious homemade cakes, freshly crafted to order just for you.",
+
     start_url: "/",
+    scope: "/",
+
     display: "standalone",
+
     background_color: "#fff7f9",
     theme_color: "#ec4899",
+
     orientation: "portrait",
+
     icons: [
       {
-        src: "/icons/icon-192.png",
+        src: "/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512.png",
+        src: "/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
-      },
-      {
-        src: "/icons/icon-512-maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
       },
     ],
   };
